f1af9d6f97a1f509de1de59b588f1ca63c98a1d7
pkhrlgnsh <pkhrlgnsh@gmail.com>
2025-03-10 18:35:30 +0545

    Assigned Tasks of OMN

---
+++ wordpress/wp-content/themes/omn/functions.php
@@ -0,0 +1,20 @@
+<!-- This codes need to be added in functons.php file -->
+<?php
+/*
+Plugin Name: Article Custom Post Type
+*/
+function create_articles_cpt() {
+    register_post_type('article', [
+        'labels' => [
+            'name' => __('Articles'),
+            'singular_name' => __('Article')
+        ],
+        'public' => true,
+        'has_archive' => true,
+        'show_in_graphql' => true,
+        'graphql_single_name' => 'article',
+        'graphql_plural_name' => 'articles',
+        'supports' => ['title', 'editor', 'thumbnail']
+    ]);
+}
+add_action('init', 'create_articles_cpt');
