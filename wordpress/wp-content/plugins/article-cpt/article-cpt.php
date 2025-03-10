1eb25e0746b90de6eb46ab73fd49f2817fe1944c
pkhrlgnsh <167727238+pkhrlgnsh@users.noreply.github.com>
2025-03-10 18:57:40 +0545

    Update article-cpt.php

---
+++ wordpress/wp-content/plugins/article-cpt/article-cpt.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name:Article Custom Post Type plugins
+ * Description: Creates 'Articles' post type with GraphQL support
+ * Author: Ganesh
+ */
+
+function create_articles_cpt() {
+    register_post_type('article', [
+        'labels' => [
+            'name' => __('Articles'),
+            'singular_name' => __('Article')
+        ],
+        'public' => true,
+        'has_archive' => true,
+        'show_in_rest' => true,
+        'show_in_graphql' => true,
+        'graphql_single_name' => 'article',
+        'graphql_plural_name' => 'articles',
+        'supports' => ['title', 'editor', 'thumbnail']
+    ]);
+}
+add_action('init', 'create_articles_cpt');
